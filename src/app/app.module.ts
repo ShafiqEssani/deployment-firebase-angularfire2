adb300193a219344417a4f5909f77daa2dcc2eff
ShafiqEssani <shafiqshamsessani@gmail.com>
2018-03-02 12:18:40 +0500

    deployment project sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UploadFormComponent } from './uploads/upload-form/upload-form.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+export const firebaseConfig = environment.firebaseConfig;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,11 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "APIKEY",
+    authDomain: "DEV-APP.firebaseapp.com",
+    databaseURL: "https://DEV-APP.firebaseio.com",
+    storageBucket: "DEV-APP.appspot.com"
+  }
 };
